d6a52a9335780d46b04d0bbeb691c8bc26d402d8
Calebe94 <calebe.sep@hotmail.com>
2017-01-20 01:37:59 -0200

    Pre-alpha stage beginnig

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from subprocess import Popen, PIPE
-import sys
-from urllib import urlencode
-from urlparse import parse_qsl
-import xbmcgui
-import xbmcplugin
-
 from urllib import urlopen
+from urllib import urlencode
+from urlparse import parse_qsl,parse_qs
+
+import sys,os, json
+import xbmcgui,xbmcplugin,xbmcaddon,xbmc
 import xml.etree.ElementTree as ET
-import os
 
 # Get the plugin url in plugin:// notation.
 _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
+
+addon = xbmcaddon.Addon()
+addon_version = addon.getAddonInfo('version')
+addon_name = addon.getAddonInfo('name')
+addonID = addon.getAddonInfo('id')
 
 VIDEOS = dict()
 LIBRARY = dict()
@@ -25,14 +28,6 @@ LINUX_GAMES = 'https://raw.githubusercontent.com/SteamDatabase/SteamLinux/master
 STEAM_WINE_PATH = "/home/calebe945/PlayOnLinux's virtual drives/calebe945/drive_c/Program Files/Steam/steamapps/"
 STEAM_LINUX_PATH = "/home/calebe945/.steam/steam/steamapps/"
 STEAM_WINE_EXECUTABLE = "/home/calebe945/PlayOnLinux's virtual drives/calebe945/drive_c/Program Files/Steam/Steam.exe"
-
-index = 0
-username = ''
-password = ''
-def Login():
-    dialog = xbmcgui.Dialog()
-    username = dialog.input('Enter your Username', type=xbmcgui.INPUT_ALPHANUM)
-    password = dialog.input('Enter your Password', type=xbmcgui.INPUT_ALPHANUM, option=xbmcgui.ALPHANUM_HIDE_INPUT)
 
 def getAppID(str):
 	str1 = str.split('_')
@@ -56,32 +51,6 @@ def getInstalledGames(path):
             AUX_DICT.setdefault('logo',[]).append({'logo':header})
     return AUX_DICT
 
-def getInstalledGames_wine(path):
-    AUX_WINE=dict()
-    for file in os.listdir(path):
-        if file.endswith(".acf"):
-            appID = getAppID(file)
-            header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%appID
-            #AUX_WINE.setdefault('WINE',[]).append({'name':getName(appID),'appID':appID,'logo':header})
-            AUX_WINE.setdefault('name',[]).append({'name':getName(appID)})
-            AUX_WINE.setdefault('appID',[]).append({'appID':appID})
-            header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%appID
-            AUX_WINE.setdefault('logo',[]).append({'logo':header})
-    return AUX_WINE
-
-def getInstalledGames_linux(path):
-    AUX_LINUX=dict()
-    for file in os.listdir(path):
-        if file.endswith(".acf"):
-            appID = getAppID(file)
-            header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%appID
-            #AUX_LINUX.setdefault('LINUX',[]).append({'name':getName(appID),'appID':appID,'logo':header})
-            AUX_LINUX.setdefault('name',[]).append({'name':getName(appID)})
-            AUX_LINUX.setdefault('appID',[]).append({'appID':appID})
-            header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%appID
-            AUX_LINUX.setdefault('logo',[]).append({'logo':header})
-    return AUX_LINUX
-
 def getOwnedGames(steamid):
     owned_games = "http://steamcommunity.com/id/%s/games?tab=all&xml=1"%steamid
     tree = ET.ElementTree(file=urlopen(owned_games))
@@ -89,7 +58,6 @@ def getOwnedGames(steamid):
     
     Owned=dict()
     for game in root.iter('game'):
-	    #Owned.setdefault('OWNED',[]).append({'name':game.find('name').text,'appID':game.find('appID').text,'logo':game.find('logo').text})
         Owned.setdefault('name',[]).append({'name':game.find('name').text})
         Owned.setdefault('appID',[]).append({'appID':game.find('appID').text})
         Owned.setdefault('logo',[]).append({'logo':game.find('logo').text})
@@ -100,8 +68,8 @@ VIDEOS.setdefault("WINE",[])
 VIDEOS.setdefault("LINUX",[])
 VIDEOS['OWNED'] = getOwnedGames("calebenovequatro")
 LIBRARY = VIDEOS['OWNED']
-VIDEOS['WINE'] = getInstalledGames_wine(STEAM_WINE_PATH)
-VIDEOS['LINUX'] = getInstalledGames_linux(STEAM_LINUX_PATH)
+VIDEOS['WINE'] = getInstalledGames(STEAM_WINE_PATH)
+VIDEOS['LINUX'] = getInstalledGames(STEAM_LINUX_PATH)
 
 def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
@@ -155,13 +123,14 @@ def play_video(path):
 
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
+    config = parse_qs(paramstring)
+    with open('/home/calebe945/STEAM_DEBUK.json', 'w') as f:
+        json.dump(config, f)
     if params:
         if params['action'] == 'listing':
             list_videos(params['category'])
         elif params['action'] == 'play':
             if params['category'] == "LINUX":
-                #oi = "steam -applaunch %s"%params['video']
-                #os.system(oi)
                 process = Popen(['steam','-applaunch',params['video'],'-silent'], stdout=PIPE, stderr=PIPE)
                 stdout, stderr = process.communicate()
                 pid = process.pid
@@ -169,11 +138,8 @@ def router(paramstring):
                 process = Popen(['wine',STEAM_WINE_EXECUTABLE,'-applaunch',params['video'],'-silent'], stdout=PIPE, stderr=PIPE)
                 stdout, stderr = process.communicate()
                 pid = process.pid
-                #oi = "wine %s -applaunch %s"%(STEAM_WINE_EXECUTABLE,params['video'])
-                #os.system(oi)
             else:
                 list_videos(params['category'])
-            #play_video(params['video'])
         else:
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
@@ -181,4 +147,9 @@ def router(paramstring):
 
 
 if __name__ == '__main__':
+    myKey = xbmcplugin.getSetting(_handle,"steam_wine")
+    myString = "addon:%s - name:%s - version:%s - hanle:%d - Setting:%s - ID:%s" % (addon,addon_name,addon_version,_handle,myKey,addonID)
+    debug = open('/home/calebe945/KODISTEAM.bgb','w')
+    debug.write(myString)
+    debug.close()
     router(sys.argv[2][1:])
