27b31419522683bddfc5bcd26401e43ea1dd775d
Calebe94 <calebe.sep@hotmail.com>
2017-02-06 19:07:07 -0200

    Version 0.0.2

--- SteamAPI.py
+++ SteamAPI.py
@@ -1,11 +1,8 @@
-from subprocess import Popen,call
+from subprocess import Popen,PIPE,call
 
 from urllib import urlopen,urlencode
 from urlparse import parse_qsl
-"""
-from urllib.request import urlopen
-from urllib.parse import urlencode,parse_qsl
-"""
+
 import sys,os,json,time
 import xml.etree.ElementTree as ET
 
@@ -53,65 +50,91 @@ def parse_acf(filename):
         return AcfNode(f)
 
 class SteamAPI(object):
-    WINE_PATH=""
-    LINUX_PATH=""
-    PUBLIC_ID=""
+    WINE_PATH=str()
+    LINUX_PATH=str()
+    PUBLIC_ID=str()
     #GET_APP_INFO = "https://steampics-mckay.rhcloud.com/info?apps=200900&prettyprint=1"
     ALL_GAMES = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=xml'
     LINUX_GAMES = 'https://raw.githubusercontent.com/SteamDatabase/SteamLinux/master/GAMES.json'
     
     LIBRARY=dict()
-
+    profilefolder=str()
     LIBRARY.setdefault("LIBRARY",[])
     LIBRARY.setdefault("WINE",[])
     LIBRARY.setdefault("LINUX",[])
     
-    def __init__(self,wine,public_id):
+    def __init__(self,wine,public_id,profiles_folder):
+        #Verification is did outside
+        profilefolder = profiles_folder
         self.LINUX_PATH=os.getenv('HOME')+"/.steam/steam/"
         self.WINE_PATH=wine
-        #self.WINE_PATH=os.getenv('HOME')+"/.PlayOnLinux/wineprefix/Steam/drive_c/Program Files/Steam/"
         self.PUBLIC_ID=public_id
-        #self.PUBLIC_ID="calebenovequatro"
         self.LIBRARY["LIBRARY"] = self.getOwnedGames()
-        self.LIBRARY["WINE"] = self.getInstalledGames(self.WINE_PATH)
-        self.LIBRARY["LINUX"] = self.getInstalledGames(self.LINUX_PATH)
+        self.LIBRARY["WINE"] = self.getInstalledGames()
+        self.LIBRARY["LINUX"] = self.getInstalledGames()
     
     def linux(self,command):
-        logfile = open("_linux_.log","a")
+        logfile = open(os.getenv("HOME")+"/_linux_.log","a")
         pid = os.fork()
+        steam = pgrep("Steam.exe")
+        if(steam):
+            os.system("wine %sSteam.exe -shutdown"%(self.path2UNIX(self.WINE_PATH)))
+            while(pgrep("Steam.exe")):
+                time.sleep(1)
         if pid == 0:
-            #os.dup2(logfile.fileno(), sys.stdout.fileno())
-            #os.dup2(sys.stdout.fileno(), sys.stderr.fileno())
-            os.system("steam -applaunch %s -silent &"%command)
+            # PIPE : stderr > stdout > logfile
+            os.dup2(logfile.fileno(),1)
+            os.dup2(1,2)
+            os.system("steam -applaunch %s -silent"%command)
             time.sleep(5)
-            sys.exit("Another Running Game: AppID:%s - Time"%command)
+            #sys.exit("Another Running Game: AppID:%s - Time"%command)
             
     def wine(self,command):
-        logfile = open("/home/calebe945/_wine_.log","a")
+        logfile = open(os.getenv("HOME")+"/_wine_.log","a")
         pid = os.fork()
+        steam = pgrep("steam")
+        if(steam):
+            os.system("steam -shutdown")
+            while(pgrep("steam")):
+                    time.sleep(1)
         if pid == 0:
-            #os.dup2(logfile.fileno(), sys.stdout.fileno())
-            #os.dup2(sys.stdout.fileno(), sys.stderr.fileno())
-            launch ="wine %sSteam.exe -applaunch %s -silent &"%(self.path2UNIX(self.WINE_PATH),command)
-            logfile.write(launch)
-            logfile.close()
-            os.system(launch)
+            # PIPE : stderr > stdout > logfile
+            os.dup2(logfile.fileno(),1)
+            os.dup2(1,2)
+            os.system("wine %sSteam.exe -applaunch %s -silent"%(self.path2UNIX(self.WINE_PATH),command))
             time.sleep(5)
-            sys.exit("Another Running Game: AppID:%s - Time"%command)
-    
-    def getInstalledGames(self,client):
+            #sys.exit("Another Running Game: AppID:%s - Time"%command)
+    #def isInstalled(appID):
+    def getInstalledGames(self):
         AUX = dict()
-        client = client+"steamapps/"
-        for file in os.listdir(client):
-            if file.endswith(".acf"):
-                acf=parse_acf(client+file)
-                AUX.setdefault('name',[]).append({'name':acf['AppState']['name']})
-                AUX.setdefault('appID',[]).append({'appID':acf['AppState']['appid']})
-                header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%acf['AppState']['appid']
-                AUX.setdefault('logo',[]).append({'logo':header})
+        """
+        AUX['linux']=dict()
+        AUX['windows']=dict()
+        if os.exists(profiles_folder+"/"+self.PUBLIC_ID+"_installed.xml"):
+            #Get Info of the xml file
+            for get all games:
+            if get platform is "windows":
+                AUX[platform] = 
+        else:
+            #continue to the following profile
+        """
+        client_wine = self.WINE_PATH + "steamapps/"
+        client_linux = self.LINUX_PATH + "steamapps/"
+        my_list ={"linux":client_linux,"windows":client_wine}
+    
+        for client in my_list:
+            for file in os.listdir(my_list[client]):
+                if file.endswith(".acf"):
+                    acf=parse_acf(my_list[client]+file)
+                    AUX.setdefault('name',[]).append({'name':acf['AppState']['name']})
+                    AUX.setdefault('appID',[]).append({'appID':acf['AppState']['appid']})
+                    header = "http://cdn.edgecast.steamstatic.com/steam/apps/%s/header.jpg"%acf['AppState']['appid']
+                    AUX.setdefault('logo',[]).append({'logo':header})
+                    AUX.setdefault('platform',[]).append({"platform":client})
         return AUX
 
     def getOwnedGames(self):
+        
         owned_games = "http://steamcommunity.com/id/%s/games?tab=all&xml=1"%self.PUBLIC_ID
         tree=ET.ElementTree(file=urlopen(owned_games))
         root = tree.getroot()
@@ -121,6 +144,7 @@ class SteamAPI(object):
             Owned.setdefault('name',[]).append({'name':game.find('name').text})
             Owned.setdefault('appID',[]).append({'appID':game.find('appID').text})
             Owned.setdefault('logo',[]).append({'logo':game.find('logo').text})
+            Owned.setdefault('platform',[]).append({'platform':"not installed"})
         return Owned
         
     def path2UNIX(self,path):
@@ -133,3 +157,34 @@ class SteamAPI(object):
             aux=aux+path[index]
             index=index+1
         return aux
+
+def pgrep(pattern):
+    pid=str()
+    process = Popen(['pgrep',pattern],stdout=PIPE,stderr=PIPE)
+    out = str(process.communicate()).split()
+    
+    if out is not None:
+        for digit in out[0]:
+            if digit.isdigit():
+                pid=pid+digit
+    return pid
+
+"""The wine() and linux() function inside the API class will return game's launching name... For that we will need to parse the game's info from the follow URL:
+https://steampics-mckay.rhcloud.com/info?apps=<appID>&prettyprint=1
+
+and then get some info from the game:
+    from common:
+        *name;
+        *logo;
+        *icon;
+        *oslist;
+        *controller_support;
+    from extended:
+        *gamedir;
+        *validoslist;
+        *pablisher;
+    from config/launch;
+        will be hard to get info from this key.
+        *executable
+
+"""
--- main.py
+++ main.py
@@ -7,44 +7,86 @@ import os
 _url = sys.argv[0]
 _handle = int(sys.argv[1])
 
-def get_url(**kwargs):
+def format_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
-def categories(categories):
-    DEBUG = open("/home/calebe945/KODISTEAM.dbg",'w')
-    command = os.getcwd()
-    DEBUG.write(command)
-    DEBUG.close()
-    for category in categories.iterkeys():
+def check_params(_steamAPI,parameters):
+    params = dict(parse_qsl(parameters))
+    if params:
+        if params['category'] == "Settings":
+            xbmcaddon.Addon().openSettings()
+        elif params['category'] == "Downloads":
+            download = xbmcgui.Dialog()
+            download.ok("NOT AVAILABLE YET!","THIS FEATURE IS IN DEVELOPMENT!")
+            main_screen(_steamAPI.LIBRARY)
+        elif params['category'] == "Installed":
+            list_games(_steamAPI,"Installed")
+            if params['action'] == 'play':
+                if params['platform'] == 'linux':
+                    _steamAPI.linux(params['game'])
+                    dialog = xbmcgui.DialogProgress()
+                    dialog.create("Xteam...","Launching the Following app ID:",params['game'])
+                    while not SteamAPI.pgrep('steam'):
+                        dialog.create("Xteam...","Launching the Following app ID:",params['game'])
+                    dialog.close()
+                elif params['platform'] == 'windows':
+                    _steamAPI.wine(params['game'])
+                else:
+                    list_games(_steamAPI,"Installed")
+            else:
+                list_games(_steamAPI,"Installed")
+        elif params['category'] == "Library":
+            list_games(_steamAPI,"Library")
+            if params['action'] == 'play':
+                library = xbmcgui.Dialog()
+                library.ok("NOT AVAILABLE!","THIS ADDON CAN'T INSTALL GAMES YET!","THIS FEATURE WILL BE AVAILABLE SOON!")
+        else:
+            raise ValueError("Invalid Paramstring:{0}!".format(parameters))
+    else:
+        main_screen(_steamAPI.LIBRARY)
+
+def main_screen(categories):
+    
+    main_categories = ["Library","Installed","Downloads","Settings"]
+    for category in main_categories:
         list_item=xbmcgui.ListItem(label=category)
-        list_item.setArt({'thumb':categories[category]["logo"][0]["logo"],'icon':categories[category]["logo"][0]["logo"],'fanart':categories[category]["logo"][0]["logo"]})
+        #list_item.setArt({'thumb':categories[category]["logo"][0]["logo"],'icon':categories[category]["logo"][0]["logo"],'fanart':categories[category]["logo"][0]["logo"]})
+        list_item.setArt({'thumb':"/home/calebe945/Imagens/LOGOS/Xteam.jpg",'icon':"/home/calebe945/Imagens/LOGOS/Xteam.jpg",'fanart':"/home/calebe945/Imagens/LOGOS/Xteam.jpg"})
         list_item.setInfo('video',{'title':category,'genre':category})
-        xbmcplugin.addDirectoryItem(_handle,get_url(action='listing', category=category),list_item,True)
+        xbmcplugin.addDirectoryItem(_handle,format_url(action='listing', category=category),list_item,True)
     xbmcplugin.addSortMethod(_handle,xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
     
-    
 def list_games(steam,categories):
-    games = steam.LIBRARY[categories]
+    #Solucao tecnica alternativa
+    games = dict()
+    if categories == "Installed":
+        games = steam.getInstalledGames()
+    if categories == "Library":
+        games = steam.getOwnedGames()
+    #elif:??? else: ??? 
+    
     for game in range(0,len(games['name'])):
         list_item = xbmcgui.ListItem(label=games['name'][game]['name'])
-        list_item.setInfo('video', {'title': games['name'][game]['name'], 'genre': games['appID'][game]['appID']})
+        list_item.setInfo('video', {'title': games['name'][game]['name'], 'genre': games['platform'][game]['platform']})
         list_item.setArt({'thumb':games['logo'][game]['logo'],'icon':games['logo'][game]['logo'],'fanart':games['logo'][game]['logo']})
-        xbmcplugin.addDirectoryItem(_handle,get_url(action='play',category=categories,game=games['appID'][game]['appID']),list_item,False)
+        xbmcplugin.addDirectoryItem(_handle,format_url(action='play',category=categories,platform=games['platform'][game]['platform'],game=games['appID'][game]['appID']),list_item,False)
     xbmcplugin.addSortMethod(_handle,xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
     
 def main(paramstring):
     params = dict(parse_qsl(paramstring))
     # First of all I need to Verify the settings before put it into the class
-    #_settings = xbmcaddon.Addon(id="plugin.video.kodisteam")
     _settings = xbmcaddon.Addon()
     dialog = xbmcgui.DialogProgress()
-    dialog.create("Xteam... Iniciando o Cliente!","Carregando Biblioteca")
+    dialog.create("Xteam...","Loading your Games")
     addonID = _settings.getAddonInfo('id')
     addonPath = _settings.getAddonInfo('path')
     userdata = xbmc.translatePath('special://profile/addon_data/'+addonID)
     profilesFolder = os.path.join(userdata,"profiles")
+    profile=open("profiles_folder.txt",'a')
+    profile.write(profilesFolder)
+    profile.close()
     #Parsing Folders
     if not os.path.isdir(userdata):
         os.mkdir(userdata)
@@ -52,23 +94,16 @@ def main(paramstring):
         os.mkdir(profilesFolder)
     WINE = _settings.getSetting("steam_wine")
     steamID = _settings.getSetting("steam_id")
-    steam = SteamAPI.SteamAPI(WINE,steamID)
+    if os.path.isdir(WINE) is not True:
+        error_wine = xbmcgui.Dialog()
+        error_wine.ok("ERROR!","WINE PATH IS NOT A VALID PATH!")
+    if os.path.isdir(os.getenv('HOME')+"/.steam/steam/") is not True:
+        error_steam = xbmcgui.Dialog()
+        error_steam.ok("ERROR!","DOES NOT FIND THE STEAM PATH","CHECK IF STEAM IS INSTALLED!")
+    steam = SteamAPI.SteamAPI(WINE,steamID,userdata)
     dialog.close()
     
-    if params:
-        if params['action'] == 'listing':
-            list_games(steam,params['category'])
-        elif params['action'] == 'play':
-            if params['category'] == "LINUX":
-                steam.linux(params['game'])
-            elif params['category'] == "WINE":
-                steam.wine(params['game'])
-            else:
-                list_games(steam,params['category'])
-        else:
-            raise ValueError("Invalid Paramstring:{0}!".format(paramstring))
-    else:
-        categories(steam.LIBRARY)
+    check_params(steam,paramstring)
 
 if __name__ == "__main__":
     main(sys.argv[2][1:])
